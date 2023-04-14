e6df25b8ae1dca5a55bfb0afb5e56de185507885
Karoline Alencar <karolinedalencar@gmail.com>
2023-04-14 19:29:16 -0300

    add fastify & fix dev configs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const logger = require('./utils/logger')
+const fastify = require('fastify')
+const logger = require('./utils/logger/index')
+
+const server = fastify()
 
 const start = () => {
   logger.info('Starting program')
+
+  server.listen({ port: 800 }, (error: Error, address: string) => {
+    if (error) {
+      logger.info(`Error at server listening: ${error}`)
+      process.exit(1)
+    }
+
+    logger.info(`Server listening at ${address}`)
+  })
 }
 
 start()
