651ef54d4540e16eec3aa1361cad8ce568189b1c
Karoline Alencar <karolinedalencar@gmail.com>
2023-04-22 20:22:08 -0300

    remove connection string

--- src/database/repository.ts
+++ src/database/repository.ts
@@ -3,8 +3,7 @@ import postgres from '@fastify/postgres'
 class DatabasRepository {
   static registerOnServer(server) {
     return server.register(postgres, {
-      connectionString:
-        'postgres://grbzqwld:gMKcTeZnWSOdSHc2yUGv7SwbCYa6n7md@baasu.db.elephantsql.com/grbzqwld'
+      connectionString: ''
     })
   }
 
