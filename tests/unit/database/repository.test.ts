823ae3d82b58ce091860b706bb7d1ea2cb2b9bc5
Karoline Alencar <karolinedalencar@gmail.com>
2023-05-03 23:17:14 -0300

    add test for database

---
+++ tests/unit/database/repository.test.ts
@@ -0,0 +1,36 @@
+import DatabaseRepository from '../../../src/database/repository'
+
+describe('registerOnServer function', () => {
+  const serverMock = {
+    register () {
+      return {
+        pg: {
+          connect () {}
+        }
+      }
+    }
+  }
+
+
+  it('should have postgres property on server', async () => {
+    const server = await DatabaseRepository.registerOnServer(serverMock)
+    expect(server).toHaveProperty('pg')
+  })
+})
+
+describe('connect function', () => {
+  const serverMock = {
+    pg: {
+      connect () {
+        return {
+          username: 'test'
+        }
+      }
+    }
+  }
+
+  it('should have postgres username property', async () => {
+    const database = await DatabaseRepository.connect(serverMock)
+    expect(database).toHaveProperty('username')
+  })
+})
